4dfbce28e947ea1013a5b39f8e316b0636c7b748
i-tereshchenko <i.tereschenko@weplay.tv>
2023-04-15 01:36:59 +0300

    - trincast/forecast

--- src/games/DOGS_6/Table/Main/ForecastTrincast/index.js
+++ src/games/DOGS_6/Table/Main/ForecastTrincast/index.js
@@ -1,13 +1,47 @@
-import Odd from "../../Odd";
-
-import style from './index.module.scss';
 import {useState} from "react";
 
+import classNames from "classnames";
+
+import Odd from "../../Odd";
+
+import Number from "../../Number";
+
+import style from './index.module.scss';
+
+const findBets = (data, id) => {
+    const r = data.filter(el => {
+        return el.a.indexOf(id) === 0
+    })
+
+    if (id.length > 1) {
+        r.splice(id[0] - 1, 0, {})
+        r.splice(id[2] - 1, 0, {})
+    }
+    else {
+        r.splice(id - 1, 0, {})
+    }
+
+    return r
+}
+
 const ForecastTrincast = ({data}) => {
-    const [select, setSelect] = useState([])
+    const [selectForecast, setForecastSelect] = useState([])
+    const [selectTrincast, setTrincastSelect] = useState([])
+    const [forecast, setForecast] = useState([])
+    const [trincast, setTrincast] = useState([])
 
-    const addStake = () => {
+    const addForecastStake = (el) => {
+        setForecastSelect([el])
 
+        setTrincast([])
+        setTrincastSelect([])
+        setForecast(findBets(data.event.g.e.g.b, el.a))
+    }
+
+    const addTrincastStake = (el) => {
+        setTrincastSelect([el])
+
+        setTrincast(findBets(data.event.g.e.h.b, el.a))
     }
 
     return (
@@ -21,15 +55,15 @@ const ForecastTrincast = ({data}) => {
                                 key={idx}
                                 className={style.cell}
                                 onClick={() => {
-                                    addStake(el)
+                                    addForecastStake(el)
                                 }}
                             >
+                                <Number
+                                    key={idx}
+                                    color={idx}
+                                    data={idx + 1}
+                                />
                                 {el.b}
-                                {/*<Odd*/}
-                                {/*    market={data.event.g.e.a.a}*/}
-                                {/*    data={el}*/}
-                                {/*    view={'horizontal'}*/}
-                                {/*/>*/}
                             </div>
                         )
                     }
@@ -37,36 +71,76 @@ const ForecastTrincast = ({data}) => {
                 <div className={style.row}>
                     <div className={style.cell}>2</div>
                     {
-                        data.event.g.e.c.b.map((el, idx) =>
-                            <div
-                                key={idx}
-                                className={style.cell}
-                            >
-                                {el.b}
-                                {/*<Odd*/}
-                                {/*    market={data.event.g.e.c.a}*/}
-                                {/*    data={el}*/}
-                                {/*    view={'horizontal'}*/}
-                                {/*/>*/}
-                            </div>
+                        forecast.map((el, idx) =>
+                            el.b
+                                ?
+                                    <div
+                                        key={idx}
+                                        className={style.cell}
+                                        onClick={() => {
+                                            addTrincastStake(el)
+                                        }}
+                                    >
+                                        <Number
+                                            key={idx}
+                                            color={idx}
+                                            data={idx + 1}
+                                        />
+                                        {el.b}
+                                    </div>
+                                :
+                                    <div
+                                        key={idx}
+                                        className={
+                                            classNames(
+                                                style.cell,
+                                                style.disabled
+                                            )
+                                        }
+                                    >
+                                        <Number
+                                            key={idx}
+                                            color={idx}
+                                            data={idx + 1}
+                                        />
+                                    </div>
+
                         )
                     }
                 </div>
                 <div className={style.row}>
                     <div className={style.cell}>3</div>
                     {
-                        data.event.g.e.c.b.map((el, idx) =>
-                            <div
-                                key={idx}
-                                className={style.cell}
-                            >
-                                {el.b}
-                                {/*<Odd*/}
-                                {/*    market={data.event.g.e.c.a}*/}
-                                {/*    data={el}*/}
-                                {/*    view={'horizontal'}*/}
-                                {/*/>*/}
-                            </div>
+                        trincast.map((el, idx) =>
+                            el.b
+                                ?
+                                    <div
+                                        key={idx}
+                                        className={style.cell}
+                                    >
+                                        <Number
+                                            key={idx}
+                                            color={idx}
+                                            data={idx + 1}
+                                        />
+                                        {el.b}
+                                    </div>
+                                :
+                                    <div
+                                        key={idx}
+                                        className={
+                                            classNames(
+                                                style.cell,
+                                                style.disabled
+                                            )
+                                        }
+                                    >
+                                        <Number
+                                            key={idx}
+                                            color={idx}
+                                            data={idx + 1}
+                                        />
+                                    </div>
                         )
                     }
                 </div>
